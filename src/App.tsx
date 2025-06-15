14e8415c9052146cbf73f1e2d87c0beb22ef27cd
shobhit-28 <shobhitraj34@gmail.com>
2025-06-15 21:00:48 +0530

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { HomePage } from './pages/homePage/homePage'
+// import { NavBar } from './components/NavBar/navBar'
 
 export default function App() {
   return (
     <>
+      {/* <NavBar /> */}
       <Routes>
         <Route path='/' element={<HomePage />} />
+        {/* <Route path='/' element={<HomePage />} /> */}
       </Routes>
     </>
   )
--- src/components/NavBar/navBar.tsx
+++ src/components/NavBar/navBar.tsx
@@ -1,5 +1,17 @@
-export const NavBar = () => {
-  return (
-    <div>navBar</div>
-  )
+import type React from "react"
+import { NavLink } from "react-router-dom"
+
+const routes: Array<{ name: string, path: string }> = [
+    { name: 'Home', path: '/' },
+    { name: 'List', path: '/' },
+]
+
+export const NavBar: React.FC = () => {
+    return (
+        <div className="w-full bg-slate-900 text-slate-400">
+            {routes.map((route) => <NavLink to={route.path} className={'nav-link'}>
+                <span className="nav-text">{route.name}</span>
+            </NavLink>)}
+        </div>
+    )
 }
